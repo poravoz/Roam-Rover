be8f4676ffaa5dfc5f2b6d6d088d86c3dddf17de
poravoz <116353344+poravoz@users.noreply.github.com>
2024-02-28 22:10:46 +0200

    Update App.tsx

--- data-entry-form/src/App.tsx
+++ data-entry-form/src/App.tsx
@@ -1,9 +1,50 @@
+import { useState } from 'react';
+import ButtonList from './Components/ButtonMenu/ButtonsList';
 import LoginSignUp from './Components/SignUp/Signup';
 
+
+const buttonsProperties = [
+  { 
+    title: 'Home',
+    color: 'black',
+    width: 60,
+    height: 20,
+  },
+  { 
+    title: 'Completed',
+    color: 'yellow',
+    width: 60,
+    height: 20,
+  },
+  { 
+    title: 'Assignments',
+    color: 'blue',
+    width: 60,
+    height: 20,
+  },
+]
+
+
+
 function App() {
+  const [clickedButton, setClickedButton] = useState('');
+
+  const elementClick =  (event: React.MouseEvent<HTMLLIElement>) => {
+    event.preventDefault();
+  
+    const leElement: HTMLLIElement = event.currentTarget;
+    setClickedButton(leElement.innerText);
+  };
+
   return (
     <div>
       <LoginSignUp/>
+      <ButtonList buttons={buttonsProperties} onClickCallback={elementClick}/>
+      <h1>
+        {
+          clickedButton !== "" ? `You have clicked "${clickedButton}"`: "No button clicked yet"
+        }
+      </h1>
     </div>
   );
 }
